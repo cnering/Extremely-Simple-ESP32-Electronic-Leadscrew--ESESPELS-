6a7ca7135ba7a3f65b849034a7af95578e5684a8
Chris Nering <chris.nering@gmail.com>
2023-08-09 15:36:47 -0400

    speeding up critical loop

--- ESP_ELS_Working_Stepper_Encoder_Display_20230807.ino
+++ ESP_ELS_Working_Stepper_Encoder_Display_20230807.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <ESP32Encoder.h>
 #include "FastAccelStepper.h"
+#include <Arduino.h>
 
 //git test
 /*!!!STEPPER DRIVER!!!*/
@@ -8,7 +9,7 @@
 #define enablePinStepper 35
 #define stepPinStepper 36
 
-#define DISPLAY_CHARACTERS 80
+int step_drives_per_second = 0;
 
 FastAccelStepperEngine engine = FastAccelStepperEngine();
 FastAccelStepper *stepper = NULL;
@@ -40,9 +41,28 @@ u_int64_t last_LCD_timer = 0;
 
 #define FEED_INCREASE_BUTTON 15
 #define FEED_DECREASE_BUTTON 5
+#define MODE_SELECT_BUTTON 17
+
+#define FEED_MODE 0
+#define TPI_MODE 1
+#define PITCH_MODE 2
+
+#define DISPLAY_REFRESH_HZ 1
+
+int last_display_refresh = 0;
 
 int feed_up_hold = 0;
 int feed_down_hold = 0;
+
+int run_mode_debounce = 0;
+
+int run_mode = 0;
+int tpi = 0;
+int pitch = 0;
+
+
+String metric_pitches[20] = {};
+
 
 
 /*!!!TASK!!!*/
@@ -62,6 +82,27 @@ u_long rpm_display_last_count = 0;
 
 void setup(){
 
+  metric_pitches[0] = "0.35";
+  metric_pitches[1] = "0.40";
+  metric_pitches[2] = "0.45";
+  metric_pitches[3] = "0.50";
+  metric_pitches[4] = "0.60";
+  metric_pitches[5] = "0.70";
+  metric_pitches[6] = "0.80";
+  metric_pitches[7] = "1.00";
+  metric_pitches[8] = "1.25";
+  metric_pitches[9] = "1.50";
+  metric_pitches[10] = "1.75";
+  metric_pitches[11] = "2.00";
+  metric_pitches[12] = "2.50";
+  metric_pitches[13] = "3.00";
+  metric_pitches[14] = "3.50";
+  metric_pitches[15] = "4.00";
+  metric_pitches[16] = "4.50";
+  metric_pitches[17] = "5.00";
+  metric_pitches[18] = "5.50";
+  metric_pitches[19] = "6.00";
+
   Serial.begin(115200);
 
   /*Input buttons*/
@@ -69,7 +110,7 @@ void setup(){
   pinMode(FEED_DECREASE_BUTTON, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
   pinMode(16, INPUT_PULLUP);
-  pinMode(17, INPUT_PULLUP);
+  pinMode(MODE_SELECT_BUTTON, INPUT_PULLUP);
 
 
   /*!!!STEPPER DRIVER!!!*/
@@ -148,18 +189,22 @@ void high_priority_loop(void * parameter){
       
       /*Set the global last_rotation to this value, MAY NOT NEED*/
       //last_rotation = rotation;
+      stepper->move(counts_delta);
+      step_drives_per_second++;
     }
     /*Debugging, just outputting the calculations per second*/
     if(cur_timer - last_timer > 1000000){
       Serial.println(calculations_per_second);
+      Serial.println(step_drives_per_second);
       calculations_per_second = 0;
+      step_drives_per_second = 0;
       last_timer = cur_timer;
     }
 
     last_count = cur_count;
 
     /*TODO: Logic to calculate stepper position from encoder rotation percentage*/
-    stepper->move(1);
+  //  stepper->move(1);
   }
 }
 
@@ -171,50 +216,67 @@ void moveStepperToPosition(int counts){
 
 void lcdUpdate(){
 
-  String encoder_pos = String("Encoder = " + String(String((int32_t)encoder.getCount())+"          ").substring(0,20));
+  last_display_refresh++;
 
-  /*first line*/
-  
-  for(int i = 0; i < 20; i++){
-    if (encoder_pos.charAt(i) != current_LCD_line_1[i]){
-      lcd.setCursor(i,0);
-      lcd.print(encoder_pos[i]);
-      current_LCD_line_1[i] = encoder_pos.charAt(i);
+  if(last_display_refresh >= 10/DISPLAY_REFRESH_HZ){
+
+    last_display_refresh = 0;
+
+    String encoder_pos = String("Encoder = " + String(String((int32_t)encoder.getCount())+"          ").substring(0,20));
+
+    /*first line*/
+    
+    for(int i = 0; i < 20; i++){
+      if (encoder_pos.charAt(i) != current_LCD_line_1[i]){
+        lcd.setCursor(i,0);
+        lcd.print(encoder_pos[i]);
+        current_LCD_line_1[i] = encoder_pos.charAt(i);
+      }
+    }
+    
+    
+    /*second line*/
+    long rpm_display_current_count = (int32_t)encoder.getCount();
+    u_int64_t display_current_update_time = esp_timer_get_time();
+    long rpm_delta = rpm_display_current_count - rpm_display_last_count;
+    u_int64_t micros_since_last_update = display_current_update_time - display_last_updated_time;
+    float seconds_since_last_update = micros_since_last_update/1000000.0;
+    float current_RPMs = rpm_delta/2400.0/seconds_since_last_update*60.0;
+    String RPMs_current = "RPMs = " + String(String(current_RPMs)+String("                    ")).substring(0,13);
+    rpm_display_last_count = rpm_display_current_count;
+    display_last_updated_time = display_current_update_time;
+    for(int i = 0; i < 20; i++){
+      if (RPMs_current.charAt(i) != current_LCD_line_2[i]){
+        lcd.setCursor(i,1);
+        lcd.print(RPMs_current[i]);
+        current_LCD_line_2[i] = RPMs_current.charAt(i);
+      }
+    }
+
+    /*third line*/
+    String sfm_current = "SFM @ 1in: "+ String(String(current_RPMs/3.82) + String("            ")).substring(0,10);
+
+    for(int i = 0; i < 20; i++){
+      if (sfm_current.charAt(i) != current_LCD_line_3[i]){
+        lcd.setCursor(i,2);
+        lcd.print(sfm_current[i]);
+        current_LCD_line_3[i] = sfm_current.charAt(i);
+      }
     }
   }
-  
-  
-  /*second line*/
-  long rpm_display_current_count = (int32_t)encoder.getCount();
-  u_int64_t display_current_update_time = esp_timer_get_time();
-  long rpm_delta = rpm_display_current_count - rpm_display_last_count;
-  u_int64_t micros_since_last_update = display_current_update_time - display_last_updated_time;
-  float seconds_since_last_update = micros_since_last_update/1000000.0;
-  float current_RPMs = rpm_delta/2400.0/seconds_since_last_update*60.0;
-  String RPMs_current = "RPMs = " + String(String(current_RPMs)+String("                    ")).substring(0,13);
-  rpm_display_last_count = rpm_display_current_count;
-  display_last_updated_time = display_current_update_time;
-  for(int i = 0; i < 20; i++){
-    if (RPMs_current.charAt(i) != current_LCD_line_2[i]){
-      lcd.setCursor(i,1);
-      lcd.print(RPMs_current[i]);
-      current_LCD_line_2[i] = RPMs_current.charAt(i);
-    }
-  }
 
-  /*third line*/
-  String sfm_current = "SFM @ 1in: "+ String(String(current_RPMs/3.82) + String("            ")).substring(0,10);
-
-  for(int i = 0; i < 20; i++){
-    if (sfm_current.charAt(i) != current_LCD_line_3[i]){
-      lcd.setCursor(i,2);
-      lcd.print(sfm_current[i]);
-      current_LCD_line_3[i] = sfm_current.charAt(i);
-    }
-  }
-
+  String feed_string = "";
   /*fourth line*/
-  String feed_string = String("Feed: 0.") +String(String("000") + String(feed_rate)).substring(String(feed_rate).length(),String(String("000") + String(feed_rate)).length()) + String("         ");
+  if(run_mode == 0){
+    feed_string = String("Feed: 0.") +String(String("000") + String(feed_rate)).substring(String(feed_rate).length(),String(String("000") + String(feed_rate)).length()) + String("         ");
+  } else if(run_mode == 1){
+    feed_string = String("TPI: ") +String(String("000") + String(tpi)).substring(String(tpi).length(),String(String("000") + String(tpi)).length()) + String("            ");
+  } else if(run_mode == 2){
+    feed_string = String("Pitch: ") + metric_pitches[pitch] + "         ";
+  } else{
+    //do nothing
+  }
+  
 
    for(int i = 0; i < 20; i++){
     if (feed_string.charAt(i) != current_LCD_line_4[i]){
@@ -240,19 +302,48 @@ void buttonCheck(){
   } else{
     feed_down_hold = 0;
   }
+  if(!digitalRead(MODE_SELECT_BUTTON)){
+    run_mode_debounce++;
+    if(run_mode_debounce == 2){
+      if(run_mode == 2){
+        run_mode = 0;
+      } else{
+        run_mode++;
+      }
+    }
+  } else{
+    run_mode_debounce = 0;
+  }
 }
 
 void feed_hold_check(int pin){
   if(pin == FEED_INCREASE_BUTTON){
     feed_up_hold++;
     if(feed_up_hold == 1 || feed_up_hold > 10){
-      feed_rate++;
+      if(run_mode == FEED_MODE){
+        feed_rate++;
+      } else if(run_mode == TPI_MODE){
+        tpi++;
+      } else if(run_mode == PITCH_MODE && pitch < 20){
+        pitch++;
+      } else{
+        //do nothing
+      }
+      
     }
   }
   if(pin == FEED_DECREASE_BUTTON){
     feed_down_hold++;
     if(feed_down_hold == 1 || feed_down_hold > 10){
-      feed_rate--;
+      if(run_mode == FEED_MODE){
+        feed_rate--;
+      } else if(run_mode == TPI_MODE){
+        tpi--;
+      } else if(run_mode == PITCH_MODE && pitch > 0){
+        pitch--;
+      } else{
+        //do nothing
+      }
     }
   }
 }
